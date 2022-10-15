4f697b10672d0dc082c8591324df9a6ebf134fef
Oluwafunbi Adeneye <adeneyeoluwafunbi@gmail.com>
2022-10-15 07:51:05 +0100

    add input for vote

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -75,6 +75,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djchat.wsgi.application'
 ASGI_APPLICATION = 'djchat.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
--- djchat/room/urls.py
+++ djchat/room/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.rooms,name='rooms')
+    path('',views.rooms,name='rooms'),
+    path('<slug>/', views.room, name='room'),
 ]
